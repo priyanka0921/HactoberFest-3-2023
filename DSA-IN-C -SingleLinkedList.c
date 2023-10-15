a816a00a086f4b2d14834c1871b8cdf6b3526d3f
Priyanka Dash <99274970+priyanka0921@users.noreply.github.com>
2023-10-15 12:15:37 +0530

    Create DSA-IN-C -SingleLinkedList.c

---
+++ DSA-IN-C -SingleLinkedList.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void printList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node* head = createNode(1);
+    head->next = createNode(2);
+    head->next->next = createNode(3);
+
+    printf("Singly Linked List: ");
+    printList(head);
+
+    return 0;
+}
